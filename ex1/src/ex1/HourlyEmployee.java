9d6dc43e6d0eec1f77af9a750573be9594ac778d
Owner-PC\Owner <g@geshto.com>
2020-03-22 17:54:25 +0200

    first targil

--- ex1/src/ex1/HourlyEmployee.java
+++ ex1/src/ex1/HourlyEmployee.java
@@ -4,5 +4,19 @@ package ex1;
 public class HourlyEmployee  extends Employee {
 	private Integer hours;
 	private Float wage;
+	
+	
+	HourlyEmployee(){
+		super();
+		this.hours = 0;
+		this.wage =(float) 0;
+	}
+	HourlyEmployee(String _FirstName, String _LastName, String _ID, Integer _hours, Float _wage ){
+		super(_FirstName, _LastName, _ID);
+		this.hours = _hours;
+		this.wage = _wage;
+		
+	}
+	
 
 }
