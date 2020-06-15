f83265cee83e5862848b05fbe26d6181380377ee
Geula Shoshan <g@geshtop.com>
2020-06-15 09:19:04 +0300

    mini project 1 final

--- MavoProject/src/renderer/Render.java
+++ MavoProject/src/renderer/Render.java
@@ -1,5 +1,6 @@
 package renderer;
 
+import elements.Camera;
 import elements.LightSource;
 import primitives.*;
 import scene.Scene;
@@ -15,6 +16,9 @@ import java.util.Random;
  * class represents a renderer for a scene
  */
 public class Render {
+	private int _threads = 1;
+	private final int SPARE_THREADS = 2;
+	private boolean _print = false;
     private static final int MAX_CALC_COLOR_LEVEL = 10;
     private static final double MIN_CALC_COLOR_K = 0.001;
     private static final double Delta = 0.1;
@@ -35,30 +39,191 @@ public class Render {
     }
     // ***************** Operations ******************** //
 
-    /**
-     * renders the scene to a 2D picture
-     */
-    public void renderImage() {
-        // for all the Y (height) pixels in the view plane
-        for (int i = 0; i < image.getNy(); ++i) {
-            // for all the X (width) pixels in the view plane
-            for (int j = 0; j < image.getNx(); ++j) {
-                // construct ray trough pixel [j,i]
-            	//יוצרים קרן דרך נקודה
-                Ray ray = scene.getCamera().constructRayThroughPixel
-                        (image.getNx(), image.getNy(), j, i,
-                                scene.getDistance(), image.getHeight(), image.getWidth());
-                //מחזירה את הנקודה הכי קרובה
-                GeoPoint closestPoint = findClosestIntersection(ray);
-                //בודקים אם נמצאה נקודה צובעים בצבע המחושב ואם לא בצבע הרקע
-                image.writePixel(j, i, closestPoint == null ? scene.getBackground().getColor() : calcColor(closestPoint, ray).getColor());
-            }
-//            double a = i;
-//            if (a > 0 && a % (image.getNy() / 100) == 0)
-//                System.out.println(Math.round((a / image.getNy()) * 100) + "%");
-        }
-    }
+    
+	/**
+	 * Pixel is an internal helper class whose objects are associated with a Render object that
+	 * they are generated in scope of. It is used for multithreading in the Renderer and for follow up
+	 * its progress.<br/>
+	 * There is a main follow up object and several secondary objects - one in each thread.
+	 * @author Dan
+	 *
+	 */
+	private class Pixel {
+		private long _maxRows = 0;
+		private long _maxCols = 0;
+		private long _pixels = 0;
+		public volatile int row = 0;
+		public volatile int col = -1;
+		private long _counter = 0;
+		private int _percents = 0;
+		private long _nextCounter = 0;
 
+		/**
+		 * The constructor for initializing the main follow up Pixel object
+		 * @param maxRows the amount of pixel rows
+		 * @param maxCols the amount of pixel columns
+		 */
+		public Pixel(int maxRows, int maxCols) {
+			_maxRows = maxRows;
+			_maxCols = maxCols;
+			_pixels = maxRows * maxCols;
+			_nextCounter = _pixels / 100;
+			if (Render.this._print) System.out.printf("\r %02d%%", _percents);
+		}
+
+		/**
+		 *  Default constructor for secondary Pixel objects
+		 */
+		public Pixel() {}
+
+		/**
+		 * Internal function for thread-safe manipulating of main follow up Pixel object - this function is
+		 * critical section for all the threads, and main Pixel object data is the shared data of this critical
+		 * section.<br/>
+		 * The function provides next pixel number each call.
+		 * @param target target secondary Pixel object to copy the row/column of the next pixel 
+		 * @return the progress percentage for follow up: if it is 0 - nothing to print, if it is -1 - the task is
+		 * finished, any other value - the progress percentage (only when it changes)
+		 */
+		private synchronized int nextP(Pixel target) {
+			++col;
+			++_counter;
+			if (col < _maxCols) {
+				target.row = this.row;
+				target.col = this.col;
+				if (_counter == _nextCounter) {
+					++_percents;
+					_nextCounter = _pixels * (_percents + 1) / 100;
+					return _percents;
+				}
+				return 0;
+			}
+			++row;
+			if (row < _maxRows) {
+				col = 0;
+				if (_counter == _nextCounter) {
+					++_percents;
+					_nextCounter = _pixels * (_percents + 1) / 100;
+					return _percents;
+				}
+				return 0;
+			}
+			return -1;
+		}
+
+		/**
+		 * Public function for getting next pixel number into secondary Pixel object.
+		 * The function prints also progress percentage in the console window.
+		 * @param target target secondary Pixel object to copy the row/column of the next pixel 
+		 * @return true if the work still in progress, -1 if it's done
+		 */
+		public boolean nextPixel(Pixel target) {
+			int percents = nextP(target);
+			if (percents > 0)
+				if (Render.this._print) System.out.printf("\r %02d%%", percents);
+			if (percents >= 0)
+				return true;
+			if (Render.this._print) System.out.printf("\r %02d%%", 100);
+			return false;
+		}
+	}
+	/**
+	 * This function renders image's pixel color map from the scene included with
+	 * the Renderer object
+	 */
+	public void renderImage() {
+		final int nX = image.getNx();
+		final int nY = image.getNy();
+
+		final Pixel thePixel = new Pixel(nY, nX);
+
+		// Generate threads
+		Thread[] threads = new Thread[_threads];
+		for (int i = _threads - 1; i >= 0; --i) {
+			threads[i] = new Thread(() -> {
+				Pixel pixel = new Pixel();
+				while (thePixel.nextPixel(pixel)) {
+					 Ray ray = scene.getCamera().constructRayThroughPixel
+		                        (image.getNx(), image.getNy(), pixel.col, pixel.row,
+		                                scene.getDistance(), image.getHeight(), image.getWidth());
+		                //מחזירה את הנקודה הכי קרובה
+		                GeoPoint closestPoint = findClosestIntersection(ray);
+		                //בודקים אם נמצאה נקודה צובעים בצבע המחושב ואם לא בצבע הרקע
+		                image.writePixel(pixel.col, pixel.row, closestPoint == null ? scene.getBackground().getColor() : calcColor(closestPoint, ray).getColor());
+
+				}
+			});
+		}
+
+		// Start threads
+		for (Thread thread : threads) thread.start();
+
+		// Wait for all threads to finish
+		for (Thread thread : threads) try { thread.join(); } catch (Exception e) {}
+		if (_print) System.out.printf("\r100%%\n");
+	}
+
+    
+    /**
+     * renders the scene to a 2D picture // before add therd
+     */
+//    public void renderImage() {
+//        // for all the Y (height) pixels in the view plane
+//        for (int i = 0; i < image.getNy(); ++i) {
+//            // for all the X (width) pixels in the view plane
+//            for (int j = 0; j < image.getNx(); ++j) {
+//                // construct ray trough pixel [j,i]
+//            	//יוצרים קרן דרך נקודה
+//                Ray ray = scene.getCamera().constructRayThroughPixel
+//                        (image.getNx(), image.getNy(), j, i,
+//                                scene.getDistance(), image.getHeight(), image.getWidth());
+//                //מחזירה את הנקודה הכי קרובה
+//                GeoPoint closestPoint = findClosestIntersection(ray);
+//                //בודקים אם נמצאה נקודה צובעים בצבע המחושב ואם לא בצבע הרקע
+//                image.writePixel(j, i, closestPoint == null ? scene.getBackground().getColor() : calcColor(closestPoint, ray).getColor());
+//            }
+//        }
+//    }
+	/**
+	 * Set multithreading <br>
+	 * - if the parameter is 0 - number of coress less 2 is taken
+	 * 
+	 * @param threads number of threads
+	 * @return the Render object itself
+	 */
+	public Render setMultithreading(int threads) {
+		if (threads < 0)
+			throw new IllegalArgumentException("Multithreading patameter must be 0 or higher");
+		if (threads != 0)
+			_threads = threads;
+		else {
+			int cores = Runtime.getRuntime().availableProcessors() - SPARE_THREADS;
+			if (cores <= 2)
+				_threads = 1;
+			else
+				_threads = cores;
+		}
+		return this;
+	}
+
+	/**
+	 * Set debug printing on
+	 * 
+	 * @return the Render object itself
+	 */
+	public Render setDebugPrint() {
+		_print = true;
+		return this;
+	}
+	/**
+	 * setter of the ray samples count
+	 *
+	 * @param sampleCount
+	 */
+	public Render setSampleCount(int sampleCount) {
+		_sampleCount = sampleCount;
+		return this;
+	}
     /**
      * prints a 2D grid on the rendered picture
      *
@@ -151,14 +316,7 @@ public class Render {
 	}
     
 
-	/**
-	 * setter of the ray samples count
-	 *
-	 * @param sampleCount
-	 */
-	public void setSampleCount(int sampleCount) {
-		_sampleCount = sampleCount;
-	}
+
 
 	public int getSampleCount() {
 		return _sampleCount;
@@ -220,7 +378,6 @@ public class Render {
 		}
 		Vector u = v.crossProduct(dir).normalize();
 
-		// HARDCODED ?!?!?!
 		Point3D pc = p0.add(dir.scale(d));
 		for (int i = this.getSampleCount() - 1; i > 0; --i) {
 			Point3D p;
--- MavoProject/src/unittests/MinitProject1Test.java
+++ MavoProject/src/unittests/MinitProject1Test.java
@@ -10,6 +10,7 @@ import elements.Camera;
 import elements.DirectionalLight;
 import elements.LightSource;
 import elements.PointLight;
+import elements.SpotLight;
 import geometries.Geometries;
 import geometries.Plane;
 import geometries.Polygon;
@@ -25,105 +26,161 @@ import scene.Scene;
 
 public class MinitProject1Test {
 
+	final int samples =15;
+
+
+	/**
+	 * integration tests for reflection and refraction
+	 */
 	@Test
-	public void MiniProject1Test() {
-		Scene scene = new Scene("MiniProject1Test");
+	public void testReflectionRefracted() {
+		Scene scene = new Scene("reflection test");
 		scene.setCamera(new Camera(new Point3D(0, 0, -1000), new Vector(0, 0, 1), new Vector(0, -1, 0)));
 
 		scene.setDistance(1000);
 		scene.setAmbient(new AmbientLight(new Color(255, 255, 255), 0));
 		scene.setBackground(Color.BLACK);
-		
-		
-
-
-		Plane plane = new Plane(new Color(0, 0, 0), new Material(0.5, 0.5, 300, 0.8, 0), new Point3D(0, 50, 60),
+		Plane plane = new Plane(new Color(0, 0, 0), new Material(0.5, 0.5, 300, 0,0.8), new Point3D(0, 50, 60),
 				new Vector(0, 1, 0));
 		Sphere sphere = new Sphere(new Color(0, 0, 20), new Material(0.5, 0.5, 300, 0.5, 0.2), 20,
-				new Point3D(0, -50, 80));
-		Sphere sphere2 = new Sphere(new Color(0, 20, 0), new Material(0.5, 0.5, 300,0, 0.2), 9,
-				new Point3D(0, -10, 80));
-		Sphere sphere3 = new Sphere(new Color(20, 20, 0), new Material(0.5, 0.5, 300, 0, 0.2), 10,
-				new Point3D(0, 10, 80));
-		Sphere sphere4 = new Sphere(new Color(20, 20, 20), new Material(0.5, 0.5, 300, 0, 0.2), 5,
 				new Point3D(0, 30, 80));
-		Sphere sphere5 = new Sphere(new Color(20, 20, 20), new Material(0.5, 0.5, 300, 0, 0.2), 40,
-				new Point3D(0, -100, 80));
-		Geometries geo1 = new Geometries(new Geometries(sphere,sphere2)
-				,new Geometries(new Geometries(sphere3,sphere4),sphere5));
-		Sphere sphere21 = new Sphere(new Color(0, 0, 20), new Material(0.5, 0.5, 300, 0.5, 0.2), 20,
-				new Point3D(-100, -50, 80));
-		Sphere sphere17 = new Sphere(new Color(0, 20, 0), new Material(0.5, 0.5, 300, 0, 0.2), 9,
-				new Point3D(-100, -10, 80));
-		Sphere sphere18 = new Sphere(new Color(20, 20, 0), new Material(0.5, 0.5, 300, 0, 0.2), 10,
-				new Point3D(-100, 10, 80));
-		Sphere sphere19 = new Sphere(new Color(20, 20, 20), new Material(0.5, 0.5, 300, 0, 0.2), 5,
-				new Point3D(-100, 30, 80));
-		Sphere sphere20 = new Sphere(new Color(20, 20, 20), new Material(0.5, 0.5, 300,0, 0.2), 40,
-				new Point3D(-100, -100, 80));
-		Geometries geo2 = new Geometries(new Geometries(sphere21,sphere17),
-				new Geometries(sphere18,sphere19),sphere20);
-		Sphere sphere22 = new Sphere(new Color(0, 0, 20), new Material(0.5, 0.5, 300, 0.5, 0.2), 20,
-				new Point3D(100, -50, 80));
-		Sphere sphere23 = new Sphere(new Color(0, 20, 0), new Material(0.5, 0.5, 300, 0, 0.2), 9,
-				new Point3D(100, -10, 30));
-		Sphere sphere24 = new Sphere(new Color(20, 20, 0), new Material(0.5, 0.5, 300, 0, 0.2), 10,
-				new Point3D(100, 10, 30));
-		Sphere sphere25 = new Sphere(new Color(20, 20, 20), new Material(0.5, 0.5, 300,0, 0.2), 5,
-				new Point3D(100, 30, 30));
-		Sphere sphere26 = new Sphere(new Color(20, 20, 20), new Material(0.5, 0.5, 300, 0, 0.2), 40,
-				new Point3D(100, -100, 30));
-		Geometries geo3 = new Geometries(new Geometries(sphere22,sphere23)
-				,new Geometries(sphere24,sphere25,sphere26));
-		Triangle triangle1 = new Triangle(new Color(20, 20, 20), new Material(0.5, 0.5, 300, 0, 0.2),//
-				new Point3D(-100, -120, 80) ,new Point3D(0, -200, 80),
-				new Point3D(100, -100, 30));
-		Sphere sphere6 = new Sphere(new Color(20, 20, 20), new Material(0.5, 0.5, 300, 0, 0.2), 40,
-				new Point3D(50, -280, 20));
-		Sphere sphere7 = new Sphere(new Color(20, 20, 20), new Material(0.5, 0.5, 300,0, 0.2), 60,
-				new Point3D(50, -120, 80));
-		Sphere sphere8 = new Sphere(new Color(20, 20, 20), new Material(0.5, 0.5, 300, 0, 0.2), 80,
-				new Point3D(50, 0, -10));
-		Sphere sphere9 = new Sphere(new Color(20, 20, 20), new Material(0.5, 0.5, 300,0, 0.2), 100,
-				new Point3D(50, 150, 80));
-		Sphere sphere10 = new Sphere(new Color(20, 20, 20), new Material(0.5, 0.5, 300,0, 0.2),80 ,
-				new Point3D(70, -280, 80));
-		Sphere sphere11 = new Sphere(new Color(20, 20, 20), new Material(0.5, 0.5, 300, 0, 0.2), 60,
-				new Point3D(70, -120, 80));
-		Sphere sphere12 = new Sphere(new Color(20, 20, 20), new Material(0.5, 0.5, 300,0 , 0.2), 40,
-				new Point3D(70, -0, 80));
-		Sphere sphere13 = new Sphere(new Color(20, 20, 20), new Material(0.5, 0.5, 300, 0, 0.2), 30,
-				new Point3D(70, 50, 80));
-		Sphere sphere14 = new Sphere(new Color(20, 20, 20), new Material(0.5, 0.5, 300, 0, 0.2), 20,
-				new Point3D(70, 70, 80));
-		Sphere sphere15 = new Sphere(new Color(20, 20, 20), new Material(0.5, 0.5, 300, 0, 0.2), 10,
-				new Point3D(70, 90, 80));
-		Sphere sphere16 = new Sphere(new Color(20, 20, 20), new Material(0.5, 0.5, 300, 0, 0.2), 1,
-				new Point3D(70, 110, 20));
-		Geometries geo5 = new Geometries(new Geometries(sphere6,sphere7,sphere8,sphere9)
-				,new Geometries(sphere10,sphere11,new Geometries(sphere12,sphere13,new Geometries(sphere14
-			     ,sphere15))),sphere16);
-		scene.addIntersectable(sphere5,sphere6,sphere7,sphere8,sphere9,sphere10,sphere11,sphere12,
-				sphere13,sphere14,sphere15,sphere16,sphere21,sphere17,sphere18
-				,sphere19,sphere20,sphere22,sphere23,sphere24,sphere25,sphere26,
-				triangle1,geo1,geo2,geo3,geo5,
-				sphere,sphere3, sphere4, sphere2,plane);//sphere);
+//		Sphere sphere2 = new Sphere(new Color(0, 20, 0), new Material(0.5, 0.5, 300, 0, 0.2), 9,
+//				new Point3D(0, 30, 80));
+		Sphere sphere3 = new Sphere(new Color(20, 20, 0), new Material(0.5, 0.5, 300, 0, 0.2), 10,
+				new Point3D(0, 0, 80));
+		Sphere sphere4 = new Sphere(new Color(20, 20, 20), new Material(0.5, 0.5, 300, 0, 0.2), 5,
+				new Point3D(0, -15, 80));
+		scene.addIntersectable(sphere3, sphere4,/*sphere2,*/ plane, sphere);
 
 		Point3D pos = new Point3D(20, -50, 0);
 		Color color = new Color(400, 300, 300);
 
 		PointLight point_light = new PointLight(color, pos, 1, 0.0005, 0.000005);
-        DirectionalLight directional_light = new DirectionalLight(color ,  new Vector(1,1,1));
-        
-		ImageWriter imageWriter = new ImageWriter("MIni project 1 Test", 2000, 2000, 1000, 1000);
+
+		ImageWriter imageWriter = new ImageWriter("spheres building (" + samples +")", 500, 500, 1200, 1200);
 		List<LightSource> lights = new ArrayList<LightSource>();
 		lights.add(point_light);
-		lights.add(directional_light);
 		scene.setLights(lights);
-		Render render = new Render(imageWriter, scene);
-		render.setSampleCount(80);
+		Render render = new Render(imageWriter, scene)
+				.setMultithreading(3) //
+				.setDebugPrint()
+				.setSampleCount(samples);
+
 		render.renderImage();
 		render.writeToImage();
 	}
+
+
+	/**
+	 * test case for matte and glossy surfaces
+	 */
+	@Test
+	public void testGlossySurface() {
+		Scene scene = new Scene("Glossy test");
+		scene.setCamera(new Camera(new Point3D(0, 0, -1000), new Vector(0, 0, 1), new Vector(0, -1, 0)));
+		scene.setDistance(1000);
+		scene.setAmbient(new AmbientLight(new Color(255, 255, 255), 0));
+		scene.setBackground(Color.BLACK);
+		Plane plane = new Plane(new Color(0, 0, 0), new Material(0.5, 0.5, 300, 0, 0.4, 0.05, 0), //
+				new Point3D(0, 20, 100), new Vector(0, 1, 0)); // ground
+		Plane plane2 = new Plane(new Color(0, 30, 30), new Material(0.5, 0.5, 1200), //
+				new Point3D(0, 20, 600), new Vector(0, 0, 1)); // background plane
+
+		Sphere sphere = new Sphere(new Color(0, 0, 20), new Material(0.5, 0.5, 1200, 0, 0.5), //
+				20, new Point3D(75, -3, 140)); // sphere 1
+		Sphere sphere2 = new Sphere(new Color(0, 0, 20), new Material(0.5, 0.5, 1200,  0, 0.5), //
+				20, new Point3D(25, -18, 140)); // sphere 2
+		Sphere sphere3 = new Sphere(new Color(0, 0, 20), new Material(0.5, 0.5, 1200,  0, 0.5), //
+				20, new Point3D(-75, -33, 140)); // sphere 3
+		Sphere sphere4 = new Sphere(new Color(0, 0, 20), new Material(0.5, 0.5, 1200,  0, 0.5), //
+				20, new Point3D(-25, -48, 140)); // sphere 4
+
+		scene.addIntersectable(plane, plane2 //
+				, sphere, sphere2, sphere3, sphere4);
+
+		Point3D pos = new Point3D(0, -30, 30);
+		Color color = new Color(400, 300, 300);
+
+		PointLight point_light = new PointLight(color, pos, 1, 0.0005, 0.000005);
+
+		ImageWriter imageWriter = new ImageWriter("Glossy test (" + samples +")", 200, 150, 600, 450);
+		List<LightSource> lights = new ArrayList<LightSource>();
+		lights.add(point_light);
+		scene.setLights(lights);
+		Render render = new Render(imageWriter, scene)
+				.setMultithreading(3) //
+				.setDebugPrint()
+				.setSampleCount(samples);
+		render.renderImage();
+		render.writeToImage();
+
+	}
+
+
+	/**
+	 * test case for matte and glossy surfaces
+	 */
+	/**
+	 * test case for matte and glossy surfaces
+	 */
+	@Test
+	public void testMatteGlass() {
+		Scene scene = new Scene("Matte test");
+		scene.setCamera(new Camera(new Point3D(0, 0, -1000), new Vector(0, 0, 1), new Vector(0, -1, 0)));
+
+		scene.setDistance(1000);
+		scene.setAmbient(new AmbientLight(new Color(255, 255, 255), 0));
+		scene.setBackground(Color.BLACK);
+
+		Plane plane = new Plane(new Color(0, 0, 0), new Material(0.5, 0.5, 300, 0, 0.4), new Point3D(0, 20, 100),
+				new Vector(0, 1, 0)); // ground Plane plane2
+		Plane plane2 = new Plane(new Color(0, 30, 30), new Material(0.5, 0.5, 1200), //
+				new Point3D(0, 20, 600), new Vector(0, 0, 1)); // background plane
+
+		Sphere sphere = new Sphere(new Color(0, 0, 20), new Material(0.5, 0.5, 1200, 0, 0.5), //
+				20, new Point3D(75, -3, 140)); // sphere 1
+		Sphere sphere2 = new Sphere(new Color(0, 0, 20), new Material(0.5, 0.5, 1200, 0, 0.5), //
+				20, new Point3D(25, -3, 140)); // sphere 2
+		Sphere sphere3 = new Sphere(new Color(0, 0, 20), new Material(0.5, 0.5, 1200,0, 0.5), //
+				20, new Point3D(-75, -3, 140)); // sphere 3
+		Sphere sphere4 = new Sphere(new Color(0, 0, 20), new Material(0.5, 0.5, 1200,0, 0.5), //
+				20, new Point3D(-25, -3, 140)); // sphere 4
+		// window for sphere 1
+		Polygon square1 = new Polygon(new Color(0, 0, 0), new Material(0.1, 0.1, 300, 0.8, 0, 0, 0.1), //
+				new Point3D(75, 20, 100), new Point3D(36, 20, 100), new Point3D(36, -40, 100),
+				new Point3D(75, -40, 100));
+		// window for sphere 2
+		Polygon square2 = new Polygon(new Color(0, 0, 0), new Material(0.1, 0.1, 300, 0.8, 0, 0, 0.5), //
+				new Point3D(1, 20, 100), new Point3D(34, 20, 100), new Point3D(34, -40, 100), new Point3D(1, -40, 100));
+		// window for sphere 3
+		Polygon square3 = new Polygon(new Color(0, 0, 0), new Material(0.1, 0.1, 300, 0.8, 0, 0, 1), //
+				new Point3D(-75, 20, 100), new Point3D(-36, 20, 100), new Point3D(-36, -40, 100),
+				new Point3D(-75, -40, 100));
+		// window for sphere 4
+		Polygon square4 = new Polygon(new Color(0, 0, 0), new Material(0.1, 0.1, 300, 0.8, 0, 0, 2), //
+				new Point3D(-1, 20, 100), new Point3D(-34, 20, 100), new Point3D(-34, -40, 100),
+				new Point3D(-1, -40, 100));
+		scene.addIntersectable(plane, plane2, //
+				sphere, sphere2, sphere3, sphere4 //
+				, square1, square2, square3, square4 //
+		);
+
+		Point3D pos = new Point3D(0, -30, 30);
+		Color color = new Color(400, 300, 300);
+
+		PointLight point_light = new PointLight(color, pos, 1, 0.0005, 0.000005);
+
+		ImageWriter imageWriter = new ImageWriter("Matte test (" + samples + ")", 200, 150, 600, 450);
+		List<LightSource> lights = new ArrayList<LightSource>();
+		lights.add(point_light);
+		scene.setLights(lights);
+		Render render = new Render(imageWriter, scene)
+				.setMultithreading(3) //
+				.setDebugPrint()
+				.setSampleCount(samples);
+		render.renderImage();
+		render.writeToImage();
+	}
+
 
 }
