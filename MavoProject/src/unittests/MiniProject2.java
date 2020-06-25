09c81bf122915689caec37fed5e3517062bfbe18
Tamar Sebbag <tamarseb@gmail.com>
2020-06-25 21:27:44 +0300

    Mini Project 2 Name "TAMAR"

--- MavoProject/src/unittests/MiniProject2.java
+++ MavoProject/src/unittests/MiniProject2.java
@@ -167,7 +167,7 @@ public class MiniProject2 {
 		
 		Geometries a_group =new Geometries(a1, a2, a3,a4);
 
-
+		// Word TAMAR: letter T
 		Polygon t1 = new Polygon(new Color(20, 20, 20), new Material(0.5, 0.5, 300, 0.2, 0), //
 				new Point3D(-1000, -450, 30),//top left
 				new Point3D(-700, -450, 30),//top right
@@ -183,12 +183,131 @@ public class MiniProject2 {
 				new Point3D(-880, -50, 30));// bottom left
 		
 		
-		
-		
-		
-		
 		Geometries t_group =new Geometries(t1,t2);
 
+		// Word TAMAR: first letter A
+		//left bar of letter A1
+		Polygon Letter_a1 = new Polygon(new Color(20, 20, 20), new Material(0.5, 0.5, 300, 0.2, 0),
+				new Point3D(-600, -50, 30),
+				new Point3D(-450, -450, 30),
+				new Point3D(-450, -350, 30),
+				new Point3D(-550, -50, 30)
+				);
+		
+		//right bar of letter A1
+		Polygon Letter_a2 = new Polygon(new Color(20, 20, 20), new Material(0.5, 0.5, 300, 0.2, 0),
+				new Point3D(-450, -450, 30),
+				new Point3D(-300, -50, 30),
+				new Point3D(-350, -50, 30),
+				new Point3D(-450, -350, 30)
+				);
+		
+		//middle bar of letter A1
+		Polygon Letter_a3 = new Polygon(new Color(20, 20, 20), new Material(0.5, 0.5, 300, 0.2, 0),
+				new Point3D(-520, -200, 30),
+				new Point3D(-370, -200, 30),
+				new Point3D(-370, -170, 30),
+				new Point3D(-520, -170, 30)
+				);
+		
+		Geometries Letter_a = new Geometries (Letter_a1,Letter_a2,Letter_a3);
+		
+		// Word TAMAR: letter M
+		//left bar of letter M
+		Polygon Letter_m1 = new Polygon(new Color(20, 20, 20), new Material(0.5, 0.5, 300, 0.2, 0),
+				new Point3D(-200, -50, 30),
+				new Point3D(-150, -50, 30),
+				new Point3D(-150, -450, 30),
+				new Point3D(-200, -450, 30)
+				);
+		
+		//left angled bar of letter M
+		Polygon Letter_m2 = new Polygon(new Color(20, 20, 20), new Material(0.5, 0.5, 300, 0.2, 0),
+				new Point3D(-150, -450, 30),
+				new Point3D(-50, -320, 30),
+				new Point3D(-50, -250, 30),
+				new Point3D(-150, -380, 30)
+				);
+		
+		//right angled bar of letter M
+		Polygon Letter_m3 = new Polygon(new Color(20, 20, 20), new Material(0.5, 0.5, 300, 0.2, 0),
+				new Point3D(-50, -320, 30),
+				new Point3D(50, -450, 30),
+				new Point3D(50, -380, 30),
+				new Point3D(-50, -250, 30)
+				);
+		
+		//right bar of letter M
+		Polygon Letter_m4 = new Polygon(new Color(20, 20, 20), new Material(0.5, 0.5, 300, 0.2, 0),
+				new Point3D(100, -50, 30),
+				new Point3D(100, -450, 30),
+				new Point3D(50, -450, 30),
+				new Point3D(50, -50, 30)
+				);
+		
+		Geometries Letter_m = new Geometries (Letter_m1,Letter_m2,Letter_m3,Letter_m4);
+		
+		// Word TAMAR: second letter A
+		//left bar of letter A2
+				Polygon Letter_A1 = new Polygon(new Color(20, 20, 20), new Material(0.5, 0.5, 300, 0.2, 0),
+						new Point3D(-600+800, -50, 30),
+						new Point3D(-450+800, -450, 30),
+						new Point3D(-450+800, -350, 30),
+						new Point3D(-550+800, -50, 30)
+						);
+				
+				//right bar of letter A2
+				Polygon Letter_A2 = new Polygon(new Color(20, 20, 20), new Material(0.5, 0.5, 300, 0.2, 0),
+						new Point3D(-450+800, -450, 30),
+						new Point3D(-300+800, -50, 30),
+						new Point3D(-350+800, -50, 30),
+						new Point3D(-450+800, -350, 30)
+						);
+				
+				//middle bar of letter A2
+				Polygon Letter_A3 = new Polygon(new Color(20, 20, 20), new Material(0.5, 0.5, 300, 0.2, 0),
+						new Point3D(-520+800, -200, 30),
+						new Point3D(-370+800, -200, 30),
+						new Point3D(-370+800, -170, 30),
+						new Point3D(-520+800, -170, 30)
+						);
+				
+				Geometries Letter_A = new Geometries (Letter_A1,Letter_A2,Letter_A3);
+				
+				// Word TAMAR: letter R
+				//left bar of letter R
+				Polygon Letter_r1 = new Polygon(new Color(20, 20, 20), new Material(0.5, 0.5, 300, 0.2, 0),
+						new Point3D(600, -50, 30),
+						new Point3D(600, -450, 30),
+						new Point3D(650, -450, 30),
+						new Point3D(650, -50, 30)
+						);
+				
+				//upper angled bar of letter R
+				Polygon Letter_r2 = new Polygon(new Color(20, 20, 20), new Material(0.5, 0.5, 300, 0.2, 0),
+						new Point3D(650, -450, 30),
+						new Point3D(870, -350, 30),
+						new Point3D(800, -350, 30),
+						new Point3D(650, -400, 30)
+						);
+				
+				//lower angled bar of letter R
+				Polygon Letter_r3 = new Polygon(new Color(20, 20, 20), new Material(0.5, 0.5, 300, 0.2, 0),
+						new Point3D(870, -350, 30),
+						new Point3D(800, -350, 30),
+						new Point3D(650, -300, 30),
+						new Point3D(650, -250, 30)
+						);
+				
+				//right foot of letter R
+				Polygon Letter_r4 = new Polygon(new Color(20, 20, 20), new Material(0.5, 0.5, 300, 0.2, 0),
+						new Point3D(650, -300, 30),
+						new Point3D(900, -50, 30),
+						new Point3D(850, -50, 30),
+						new Point3D(650, -250, 30)
+						);
+				
+				Geometries Letter_r = new Geometries (Letter_r1,Letter_r2,Letter_r3,Letter_r4);
 		
 		Triangle tr1=  new Triangle(Color.BLACK, new Material(0.8, 0.2, 300),
                   new Point3D(-450, 150, 150), new Point3D(-150, 150, 150), new Point3D(-75, -75, 150));
@@ -208,7 +327,11 @@ public class MiniProject2 {
 				u_group,
 				l_group,
 				a_group,	
-				t_group
+				t_group,
+				Letter_a,
+				Letter_m,
+				Letter_A,
+				Letter_r
 				);
 		
 		Point3D pos = new Point3D(20, -50, 0);
