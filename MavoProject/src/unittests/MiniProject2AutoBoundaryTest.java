9b2c5c1a4567050c7c069aa42d6e792b8eafedd4
Geula Shoshan <g@geshtop.com>
2020-06-26 15:56:43 +0300

    mini project 2 make test image

--- MavoProject/src/unittests/MiniProject2AutoBoundaryTest.java
+++ MavoProject/src/unittests/MiniProject2AutoBoundaryTest.java
@@ -47,68 +47,102 @@ public class MiniProject2AutoBoundaryTest {
 
 		int position = 0;
 		Polygon main_rectangle = new Polygon(new Color(20, 20, 20), new Material(0.5, 0.5, 300, 0, 0), //
-				new Point3D(-350, -500, position),//top left 
-				new Point3D(350, -500, position),//top right
-				new Point3D(350, 200, position), //bottom right
-				new Point3D(-350, 200, position)); // bottom left
+				new Point3D(-850, 350, position),//top left 
+				new Point3D(-350, 350, position),//top right
+				new Point3D(-350, 800, position), //bottom right
+				new Point3D(-850, 800, position)); // bottom left
 		Geometries main_rectangle_group =new Geometries(main_rectangle);
-		
-		
-		Sphere center_sphare = new Sphere(new Color(0, 0, 20), new Material(0.5, 0.5, 300, 0.5, 0.5), 100,
-				new Point3D(0, -150, position+50));
-		Geometries center_sphare_group =new Geometries(center_sphare);
-
-		Sphere top_sphare = new Sphere(new Color(0, 0, 20), new Material(0.5, 0.5, 100,  0, 0.5), 80,
-				new Point3D(50, -600, position-10));
-		Geometries top_sphare_group =new Geometries(top_sphare);
-		
-		
-		Triangle main_bottom_triangle =   new Triangle(new Color(0, 0, 20), new Material(0.8, 0.2, 300),
-		          new Point3D(-350, 200, position),
-		          new Point3D(350, 200, position),
-		          new Point3D(0, 700, position+20));
-		Geometries main_bottom_triangle_group =new Geometries(main_bottom_triangle);
-		
-
-		Polygon side_top_rectangle = new Polygon(new Color(0, 20, 20), new Material(0.5, 0.5, 300, 0, 0), //
-				new Point3D(-100, -650, position-40),//top left 
-				new Point3D(500, -650, position-40),//top right
-				new Point3D(350, -500, position), //bottom right
-				new Point3D(-350, -500, position)); // bottom left
-		Geometries side_top_rectangle_group =new Geometries(side_top_rectangle);
-		
-		
-		Polygon side_right_rectangle = new Polygon(new Color(0, 10, 20), new Material(0.5, 0.5, 300, 0, 0), //
-				new Point3D(350, -500, position),//top left 
-				new Point3D(500, -650, position-40),//top right
-				new Point3D(500, 50, position-40), //bottom right
-				new Point3D(350, 200, position)); // bottom left
+		Polygon side_right_rectangle = new Polygon(new Color(0, 20, 20), new Material(0.5, 0.5, 300, 0, 0), //
+				new Point3D(-350, 350, position),//top left 
+				new Point3D(100, -50, position),//top right
+				new Point3D(100, 300, position), //bottom right
+				new Point3D(-350, 800, position)); // bottom left
 		Geometries side_right_rectangle_group =new Geometries( side_right_rectangle);
 		
 		
-		Triangle side_right_triangle =   new Triangle(new Color(0, 0, 20), new Material(0.8, 0.2, 300),
-		          new Point3D(350, 200, position),
-		          new Point3D(500, 50, position-40),
-		          new Point3D(0, 700, position+20));
-		Geometries side_right_triangle_group =new Geometries( side_right_triangle);
+		Triangle main_top_triangle =   new Triangle(new Color(0, 0, 20), new Material(0.8, 0.2, 300),
+		          new Point3D(-850, 350, position),
+		          new Point3D(-350, 350, position),
+		          new Point3D(-550, 0, position));
+		Geometries main_top_triangle_group =new Geometries(main_top_triangle);
 		
-		Triangle out_triangle =   new Triangle(new Color(0, 0, 20), new Material(0.8, 0.2, 300),
-		          new Point3D(-1000, 700, position),
-		          new Point3D(-700, 1000, position),
-		          new Point3D(-1000, 1000, position));
 		
-		Geometries out_triangle_group =new Geometries( out_triangle);
+//		Triangle second_top_triangle =   new Triangle(new Color(30, 0, 20), new Material(0.8, 0.2, 300),
+//		          new Point3D(-350, 350, position),
+//		          new Point3D(100, -50, position),
+//		          new Point3D(-550, 0, position));
+//		Geometries second_top_triangle_group =new Geometries(second_top_triangle);
+		
+		Polygon side_top_rectangle = new Polygon(new Color(0, 10, 20), new Material(0.5, 0.5, 300, 0, 0), //
+				new Point3D(-550, 0, position),//top left 
+				new Point3D(-50, -250, position),//top right
+				new Point3D(100, -50, position), //bottom right
+				new Point3D(-350, 350, position)); // bottom left
+		Geometries side_top_rectangle_group =new Geometries(side_top_rectangle);
+		
+		
+		
+//		Sphere center_sphare = new Sphere(new Color(0, 0, 20), new Material(0.5, 0.5, 300, 0.5, 0.5), 100,
+//				new Point3D(0, -150, position+50));
+//		Geometries center_sphare_group =new Geometries(center_sphare);
 
+//		Sphere top_sphare = new Sphere(new Color(0, 0, 20), new Material(0.5, 0.5, 100,  0, 0.5), 80,
+//				new Point3D(50, -600, position-10));
+//		Geometries top_sphare_group =new Geometries(top_sphare);
 		
-		Geometries main_group = new Geometries (main_rectangle_group,
-				center_sphare_group,
-				top_sphare_group,
-				main_bottom_triangle_group,
-				side_top_rectangle_group,
+		
+		
+		
+
+		Plane plane = new Plane(new Color(0, 0, 0), new Material(0.5, 0.5, 300, 0.8, 0), new Point3D(0, 800, 60),
+				new Vector(0, 1, 0));
+		Sphere sphere = new Sphere(new Color(0, 0, 20), new Material(0.5, 0.5, 300, 0.2, 0.5), 20,
+				new Point3D(-700, -50, 80));
+		Sphere sphere2 = new Sphere(new Color(0, 20, 0), new Material(0.5, 0.5, 300, 0.2, 0), 9,
+				new Point3D(-700, -10, 80));
+		Sphere sphere3 = new Sphere(new Color(20, 20, 0), new Material(0.5, 0.5, 300, 0.2, 0), 10,
+				new Point3D(-700, 10, 80));
+		Sphere sphere4 = new Sphere(new Color(20, 20, 20), new Material(0.5, 0.5, 300, 0.2, 0), 5,
+				new Point3D(-700, 30, 80));
+		Sphere sphere5 = new Sphere(new Color(20, 20, 20), new Material(0.5, 0.5, 300, 0.2, 0), 40,
+				new Point3D(-700, -100, 80));
+	
+		Sphere sphere21 = new Sphere(new Color(0, 0, 20), new Material(0.5, 0.5, 300, 0.2, 0.5), 20,
+				new Point3D(900, -50, 80));
+		Sphere sphere17 = new Sphere(new Color(0, 20, 0), new Material(0.5, 0.5, 300, 0.2, 0), 9,
+				new Point3D(900, -10, 80));
+		Sphere sphere18 = new Sphere(new Color(20, 20, 0), new Material(0.5, 0.5, 300, 0.2, 0), 10,
+				new Point3D(900, 10, 80));
+		Sphere sphere19 = new Sphere(new Color(20, 20, 20), new Material(0.5, 0.5, 300, 0.2, 0), 5,
+				new Point3D(900, 30, 80));
+		Sphere sphere20 = new Sphere(new Color(20, 20, 20), new Material(0.5, 0.5, 300, 0.2, 0), 40,
+				new Point3D(900, -100, 80));
+		
+		
+		
+		
+		Geometries main_group = new Geometries (
+				main_rectangle_group,
 				side_right_rectangle_group,
-				side_right_triangle_group,
-				out_triangle,
-				out_triangle_group);
+				main_top_triangle_group,
+				side_top_rectangle_group,
+				sphere,sphere2,
+				sphere3,sphere4,
+				sphere5,
+				sphere21,sphere17,
+				sphere18,sphere19,
+				sphere20,
+				plane
+				
+				//center_sphare_group,
+				//top_sphare_group,
+				//main_bottom_triangle_group,
+				
+				
+				//side_right_triangle_group,
+				//out_triangle,
+				//out_triangle_group
+				);
 		
 				main_group.groupGeometries(10);
 		scene.addIntersectable(
@@ -118,28 +152,28 @@ public class MiniProject2AutoBoundaryTest {
 				);
 		
 		
-		boolean b1 = doOverlap(main_rectangle_group, center_sphare_group);
-		assertEquals("המרובע הראשי והעיגול הפנימי מכילים אחד את השני",b1, true); 
-
-		boolean b2 = doOverlap(main_rectangle_group, out_triangle_group);
-		assertEquals("המרובע הראשי והמשולש החיצוני לא חופפים",b2, false); 
+//		boolean b1 = doOverlap(main_rectangle_group, center_sphare_group);
+//		assertEquals("המרובע הראשי והעיגול הפנימי מכילים אחד את השני",b1, true); 
+//
+//		boolean b2 = doOverlap(main_rectangle_group, out_triangle_group);
+//		assertEquals("המרובע הראשי והמשולש החיצוני לא חופפים",b2, false); 
+//		
+//		boolean b3 = doOverlap(main_rectangle_group, main_bottom_triangle_group);
+//		assertEquals("המרובע הראשי והמשולש הראשי חופפים בנקודות קצה",b3, true); 
+//		
+//		boolean b4 = doOverlap(main_rectangle_group, top_sphare_group);
+//		assertEquals("מרובע ראשי ועיגול עליון לא חופפים",b4, false); 
+//		
+//		boolean b5 = doOverlap(side_top_rectangle_group, top_sphare_group);
+//		assertEquals("מרובע עליון ועיגול עליון עם נקודות חפיפה",b5, true); 
+//		
+//		boolean b6 = doOverlap(bg_plane_group, top_sphare_group);
+//		assertEquals("למשטח יש נקודות חפיפה עם כל האלמנטים",b6, true); 
 		
-		boolean b3 = doOverlap(main_rectangle_group, main_bottom_triangle_group);
-		assertEquals("המרובע הראשי והמשולש הראשי חופפים בנקודות קצה",b3, true); 
-		
-		boolean b4 = doOverlap(main_rectangle_group, top_sphare_group);
-		assertEquals("מרובע ראשי ועיגול עליון לא חופפים",b4, false); 
-		
-		boolean b5 = doOverlap(side_top_rectangle_group, top_sphare_group);
-		assertEquals("מרובע עליון ועיגול עליון עם נקודות חפיפה",b5, true); 
-		
-		boolean b6 = doOverlap(bg_plane_group, top_sphare_group);
-		assertEquals("למשטח יש נקודות חפיפה עם כל האלמנטים",b6, true); 
 		
 		
-		
-		double d1 = getDistance(main_rectangle_group, out_triangle_group);
-		double d2 = getDistance(main_rectangle_group, top_sphare_group);
+		//double d1 = getDistance(main_rectangle_group, out_triangle_group);
+		//double d2 = getDistance(main_rectangle_group, top_sphare_group);
 
 		Point3D pos = new Point3D(0, -160, -700);
 		Color color = new Color(400, 300, 300);
@@ -149,15 +183,15 @@ public class MiniProject2AutoBoundaryTest {
         
 		ImageWriter imageWriter = new ImageWriter("BVH Distance test", 2000, 2000, 1000, 1000);
 		List<LightSource> lights = new ArrayList<LightSource>();
-		lights.add(point_light);
-		//lights.add(directional_light);
-		//lights.add(new DirectionalLight(new Color(300, 150, 150), new Vector(1, 0, 1)));
+		//lights.add(point_light);
+		lights.add(directional_light);
+		lights.add(new DirectionalLight(new Color(300, 150, 150), new Vector(0, 0, 1)));
 //		lights.add(new PointLight(new Color(500, 250, 250),
 //                new Point3D(10, 10, 130),
 //                1, 0.0005, 0.0005));
-////		lights.add(new SpotLight(new Color(500, 250, 250),
-////	                new Point3D(10, 10, 130), new Vector(-2, 2, 1),
-////	                1, 0.0001, 0.000005));
+//		lights.add(new SpotLight(new Color(500, 250, 250),
+//	                new Point3D(10, 10, 130), new Vector(-2, 2, 1),
+//	                1, 0.0001, 0.000005));
 		scene.setLights(lights);
 		Render render = new Render(imageWriter, scene)
 				//.setMultithreading(2)
